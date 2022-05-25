bebcfa4200874be928cd4462d7ba2c0ad3b82b32
gyaanwxotica <103824607+gyaanwxotica@users.noreply.github.com>
2022-05-25 12:09:26 +0530

    Add files via upload

---
+++ trash.js
@@ -0,0 +1,23 @@
+class trash{
+
+
+
+
+
+
+
+
+    constructor(x,y,r){
+
+        this.x=x;
+        this.y=y;
+        this.r=r;
+     
+        this.body= Bodies.circle(this.x,this.y, (this.r)/2,options)
+        World.add(World,this.body)}
+
+
+
+
+
+    }
